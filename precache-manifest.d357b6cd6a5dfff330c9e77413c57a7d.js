8cb1875fe2372696d2877422945666db2ec46f25
Polina Slyusareva <ghileors@gmail.com>
2020-08-06 14:33:55 +0300

    Updates

---
+++ precache-manifest.d357b6cd6a5dfff330c9e77413c57a7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc45ab2bf8f0668fd192f3f317989c6f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "bf309bbd04e6d495e225",
+    "url": "/goit-react-hw-01-components/static/css/2.972ec59c.chunk.css"
+  },
+  {
+    "revision": "2a2ea079cc500ecf1040",
+    "url": "/goit-react-hw-01-components/static/css/main.97952d8a.chunk.css"
+  },
+  {
+    "revision": "bf309bbd04e6d495e225",
+    "url": "/goit-react-hw-01-components/static/js/2.b699e400.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.b699e400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2ea079cc500ecf1040",
+    "url": "/goit-react-hw-01-components/static/js/main.8dc82826.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
