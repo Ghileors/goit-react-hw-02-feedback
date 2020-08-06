47374c6f73b46d2ed598641ca49f41f61fcc7882
Polina Slyusareva <ghileors@gmail.com>
2020-08-06 14:43:59 +0300

    Updates

---
+++ precache-manifest.186a7af87411ca1518ffc393db3f488e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c81e8512dcccc6a1b8156cdb80c10b",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "bf309bbd04e6d495e225",
+    "url": "/goit-react-hw-02-feedback/static/css/2.972ec59c.chunk.css"
+  },
+  {
+    "revision": "5bb945f7c083c73664f9",
+    "url": "/goit-react-hw-02-feedback/static/css/main.97952d8a.chunk.css"
+  },
+  {
+    "revision": "bf309bbd04e6d495e225",
+    "url": "/goit-react-hw-02-feedback/static/js/2.b699e400.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.b699e400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb945f7c083c73664f9",
+    "url": "/goit-react-hw-02-feedback/static/js/main.e51397fb.chunk.js"
+  },
+  {
+    "revision": "b58a435a92ac135c88f5",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.82cfee65.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.5b78d188657c800d5d4c3108d6115233.js"
+  "/goit-react-hw-02-feedback/precache-manifest.186a7af87411ca1518ffc393db3f488e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-02-feedback/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
