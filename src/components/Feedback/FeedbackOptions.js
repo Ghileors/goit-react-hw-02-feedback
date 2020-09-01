4f93a2aa172c1e7377b2015e493a638881fa01cf
Polina Slyusareva <ghileors@gmail.com>
2020-09-01 14:08:07 +0300

    Refactor.

--- src/components/Feedback/FeedbackOptions.js
+++ src/components/Feedback/FeedbackOptions.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Button from './Button';
+
 import style from './Feedback.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => (
